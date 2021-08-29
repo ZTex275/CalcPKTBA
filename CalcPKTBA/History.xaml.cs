aeef9aabd2eb0ff3bdb74a3baef6532ac186278d
Ilya Buzunov <ilia.buzunov@mail.ru>
2021-08-29 12:53:51 +0300

    Добавьте файлы проекта.

---
+++ CalcPKTBA/History.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.ComponentModel;
+
+namespace CalcPKTBA
+{
+    /// <summary>
+    /// Логика взаимодействия для History.xaml
+    /// </summary>
+    public partial class History : Window
+    {
+        public string calculationsHistory;
+        private readonly string path = @"C:\\calcHistory.txt";
+
+        public History()
+        {
+            InitializeComponent();
+            readFromTextFile();
+        }
+        public void getHistory()
+        {
+            historyBlock.Text += calculationsHistory + "\n";
+            writeToTextFile();
+        }
+
+        public void writeToTextFile()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8); // Логика записи в файл: записывается все что в этом окне
+                sw.Write(historyBlock.Text);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Выполнение блока finally.");
+            }
+        }
+
+        public void readFromTextFile()
+        {
+            try
+            {
+                historyBlock.Text = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Выполнение блока finally.");
+            }
+        }
+
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8); // Логика записи в файл: записывается все что в этом окне
+                sw.Write("");
+                sw.Close();
+
+                historyBlock.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Выполнение блока finally.");
+            }
+        }
+
+        private void History_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ CalcPKTBA/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalcPKTBA
+{
+    public partial class MainWindow : Window
+    {
+        string leftop = "";    // Левый операнд
+        string operation = ""; // Знак операции
+        string rightop = "";   // Правый операнд
+
+        History historyWindow = new History();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Добавляем обработчик для всех кнопок на гриде
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button button)
+                {
+                    button.Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки
+            string s = (string)((Button)e.OriginalSource).Content;
+
+            if(textBlock.Text.Contains("="))
+            {
+                textBlock.Text = "";
+            }
+
+            if (s != "История")
+            {
+                // Добавляем его в текстовое поле
+                textBlock.Text += s;
+
+                //Не позволяет размещать одинаковые символы
+                if (textBlock.Text.Contains("--") || textBlock.Text.Contains("++") || textBlock.Text.Contains("//") || textBlock.Text.Contains("**"))
+                {
+                    textBlock.Text = textBlock.Text.Remove(textBlock.Text.Length - 1);
+                }
+            }
+
+            // Пытаемся преобразовать в число
+            bool result = int.TryParse(s, out _);
+
+            // Если текст - это число
+            if (result)
+            {
+                // Если операция не задана
+                if (operation == "")
+                {
+                    // Добавляем к левому операнду
+                    leftop += s;
+                }
+                else
+                {
+                    // Иначе к правому операнду
+                    rightop += s;
+                }
+            }
+            // Если было введено не число
+            else
+            {
+                // Если равно, то выводим результат операции
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    textBlock.Text += rightop;
+                    operation = "";
+                    rightop = "";
+
+                    historyWindow.calculationsHistory = textBlock.Text;
+                    historyWindow.getHistory();
+                }
+                // Очищаем поле и переменные
+                else if (s == "Очистить")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    textBlock.Text = "";
+                }
+                // Получаем операцию
+                else
+                {
+                    // Если правый операнд уже имеется, то присваиваем его значение левому операнду, а правый операнд очищаем
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+        // Обновляем значение правого операнда
+        private void Update_RightOp()
+        {
+            int num1, num2;
+
+            if (leftop != string.Empty)
+            {
+                num1 = int.Parse(leftop);
+            }
+            else
+            {
+                return;
+            }
+
+            if (rightop != string.Empty)
+            {
+                num2 = int.Parse(rightop);
+            }
+            else
+            {
+                num2 = num1;
+                rightop = num1.ToString();
+            }
+
+            // Выполняем операцию
+            switch (operation)
+            {
+                case "+":
+                    rightop = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightop = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightop = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightop = num2 != 0 ? (num1 / num2).ToString() : "Невозможная операция";
+                    break;
+            }
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            // MainWindow главное окно для taskWindow
+            historyWindow.Owner = this;
+            historyWindow.Show();
+        }
+    }
+}
